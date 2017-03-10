c283eada4073eb85199c4a424b1ff653a447ca05
zhangdiwen <zhangdiwen32@163.com>
2017-03-10 10:35:33 +0800

    update

--- src/reducers/HomePage.js
+++ src/reducers/HomePage.js
@@ -3,31 +3,37 @@
  */
 import {SELECT_TAB, RECEIVE_TOPICS} from '../actions';
 
-const selectedTab = (state = {selectedTab: 'all'}, action) => {
+const selectedTab = (state, action) => {
     switch (action.type) {
         case SELECT_TAB:
+            return action.tab
+        default:
+            return state;
+    }
+};
+
+const tabItem = (state = {isFetching: false, page: 0, items: []}, action) => {
+    switch (action.type) {
+        case RECEIVE_TOPICS:
             return {
                 ...state,
-                selectedTab: action.tab
+                isFetching: false,
+                page: action.page,
+                items: action.items,
+                limit: action.limit
             };
         default:
             return state;
     }
 };
 
-// const tabItem = (state = {isFetching: false, page: 0, items: []}, action) => {
-//     switch (action.type) {
-//         case RECEIVE_TOPICS:
-//             return {
-//                 ...state,
-//                 isFetching: false,
-//                 page: action.page,
-//                 items: action.items,
-//                 limit: action.limit
-//             };
-//         default:
-//             return state;
-//     }
-// };
+const homepage = (state={selectedTab:'all',tabData:{}},action) => {
+    if(state){
+        const _selectedTab = selectedTab(state.selectedTab,action);
+        const _tabItem = tabItem(state.tabData,action);
+        return {...state,selectedTab:_selectedTab,tabItem:_tabItem}
+    }
+    return state;
+};
 
-export default selectedTab;
+export default homepage;
