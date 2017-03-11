2c606c2363c977f26ec58ff9e36dce5ab474676f
zhangdiwen <zhangdiwen32@163.com>
2017-03-11 19:25:03 +0800

    update reply

--- src/components/article/reply/reply.js
+++ src/components/article/reply/reply.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import styles from './style.scss';
+import {getTime} from '../../../utils/utils';
 
 class Reply extends Component{
     constructor(props){
@@ -7,13 +8,28 @@ class Reply extends Component{
     }
 
     render(){
+        const {replyCount,replyData=[]} = this.props;
         return (
             <div className={styles.reply}>
                 <div className={styles.replyCount}>
-                    回复：{this.props.replyCount}
+                    回复：{replyCount}
                 </div>
                 <div className={styles.replyContent}>
-                    
+                    {replyData.map((value,index)=>{
+                        return (
+                            <div  key={`replyKey${index}`} style={{borderBottom:'1px solid #f6f6f6'}}>
+                                <div className={styles.author}>
+                                    <img src={value.author.avatar_url} alt="评论者"/>
+                                    <span style={{paddingLeft:'1rem',color:'#666',fontSize:'0.75rem'}}>{value.author.loginname}</span>
+                                    <span style={{paddingLeft:'.5rem',color:'#08c',fontSize:'0.75rem'}}>{index+1}楼</span>
+                                    <span style={{color:'#08c',fontSize:'0.75rem'}}>•{getTime(value.create_at)}</span>
+                                </div>
+                                <div dangerouslySetInnerHTML={{__html:value.content}}>
+
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
